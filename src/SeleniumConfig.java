f7336d4b21a888d69b4efc396c8a30004a7a6ce8
Kris <kristof.solak@gmail.com>
2022-07-05 16:32:52 +0100

    Managed to click on facebook's cookies

--- src/SeleniumConfig.java
+++ src/SeleniumConfig.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -24,11 +26,22 @@ public class SeleniumConfig {
        driver.findElement(By.id("js-link-box-en")).click();
 */
 
-        // Click on cookie
+/*        // Click on cookie
         driverGet(driver,"https://www.amazon.co.uk/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
        driver.findElement(By.id("a-autoid-0")).click();
-
+*/
+        // Click on cookies
+        driverGet(driver, "https://en-gb.facebook.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        List<WebElement> elements = driver.findElements(By.tagName("button"));
+        elements.forEach(webElement -> {
+            System.out.println(webElement.getText());
+            if (webElement.getText().equalsIgnoreCase("Allow essential and optional cookies")){
+              System.out.println("sikerult");
+              webElement.click();
+            }
+        });
     }
     protected static void driverGet(WebDriver driver, String url) {driver.get(url);}
 }
