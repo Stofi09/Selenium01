a98b750826e6b6e3271d6a974bc530e8a44a49fe
Kris <kristof.solak@gmail.com>
2022-07-05 16:17:11 +0100

    Learning findElement method

--- src/SeleniumConfig.java
+++ src/SeleniumConfig.java
@@ -1,6 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumConfig {
 
@@ -19,11 +24,11 @@ public class SeleniumConfig {
        driver.findElement(By.id("js-link-box-en")).click();
 */
 
-
-
+        // Click on cookie
+        driverGet(driver,"https://www.amazon.co.uk/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       driver.findElement(By.id("a-autoid-0")).click();
 
     }
-    protected static void driverGet(WebDriver driver, String url){
-        driver.get(url);
-    }
+    protected static void driverGet(WebDriver driver, String url) {driver.get(url);}
 }
